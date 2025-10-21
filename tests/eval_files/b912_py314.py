c5947fb6797d6bad04e62d60b2c6933e6593a90f
Cooper Ry Lees <me@cooperlees.com>
2025-10-20 20:26:02 -0500

    Drop 3.9 support + add 3.14 CI
    - Update pyproject.toml etc.
    - Add to CI runs
    - Drop 3.9 as flake8 itself did
    - Remove expected error in b912_py314.py ...
      Can we double check that makes sense ...
    
    Test:
    - Run tox in 3.14
      - `python3 -m venv /tmp/tf --upgrade-deps`
      - `/tmp/tf/bin/pip install tox`
      - `/tmp/tf/bin/tox -e py314`
    - Repeat in 3.13

--- tests/eval_files/b912_py314.py
+++ tests/eval_files/b912_py314.py
@@ -1,6 +1,6 @@
 map(lambda x: x, "a", "b")  # B912: 0
 map(lambda x: x, "a", "b", *map("c"))  # B912: 0
-map(lambda x: x, "a", map(lambda x: x, "a"), strict=True)  # B912: 22
+map(lambda x: x, "a", map(lambda x: x, "a"), strict=True)
 
 map()
 map(lambda x: x, "a")
